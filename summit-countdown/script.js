634e3ea9e92c6b0e0375e9c993ca42976653020f
awanshrestha <awanshrestha1@gmail.com>
2021-02-19 13:34:50 +0545

    adds countdown

---
+++ summit-countdown/script.js
@@ -0,0 +1,24 @@
+let eventTime = new Date('Fri Feb 26 2021 08:00:00 GMT-0800').getTime();
+
+        let x = setInterval(function() {
+        let now = new Date().getTime();
+        let difference = eventTime - now;
+
+        let days = Math.floor(difference / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+        // document.getElementById("timer").innerHTML = days + " Days " + hours + " Hours "
+        // + minutes + " Minutes " + seconds + " Seconds ";
+
+        document.getElementById("daystogo").innerHTML = days;
+        document.getElementById("hourstogo").innerHTML = hours;
+        document.getElementById("minutestogo").innerHTML = minutes;
+        document.getElementById("secondstogo").innerHTML = seconds;
+
+        if (difference < 0) {
+            clearInterval(x);
+            document.getElementById("timer").innerHTML = "Finally! The Summit has begun";
+        }
+        }, 1000);
